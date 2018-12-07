3fd2ba0523dc04673c2df7c5602728a2ee926951
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-07 12:26:14 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT into bears (name, age, color ) VALUES ("Mr Chocolate", 4, "brown");
+INSERT into bears (name) VALUES ("ROWDY");
+INSERT into bears (name) VALUES ("Tabitha");
+INSERT into bears (name) VALUES ("Sergeant Brown");
+INSERT into bears (name) VALUES ("Melissa");
+INSERT into bears (name) VALUES ("Grinch");
+INSERT into bears (name) VALUES ("Wendy");
+INSERT into bears (name) VALUES (NULL);
